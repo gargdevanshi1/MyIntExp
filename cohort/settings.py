d106a0460766c81971a51a84148fe4b25747cdbb
Devanshi Garg <gargdevanshi1@gmail.com>
2021-11-28 00:23:01 +0530

    Updated settings

--- cohort/settings.py
+++ cohort/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "68373a3785e42037e52cdde890897f539c7d2ff2d1bf2f6b"
+SECRET_KEY = os.environ.get(['SECRET_KEY'])
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
