e58052717d72e9e76b3ba94cb8d8d563ef983bde
Devanshi Garg <gargdevanshi1@gmail.com>
2021-11-27 15:48:07 +0530

    Added env variables

--- cohort/settings.py
+++ cohort/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fn9!e9a!=^i0v=%0*@tyo$h6t$q30mnkp6klhmg0x*jm=lxbq('
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['myintexp.herokuapp.com']
 
 
 # Application definition
